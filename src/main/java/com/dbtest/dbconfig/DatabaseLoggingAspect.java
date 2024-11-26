d867a914386b92c0619fef6bec6e1cc5166d3857
shahrestanaki <shahrestanaky_sh@yahoo.com>
2024-11-26 15:22:08 +0330

    add connection info

---
+++ src/main/java/com/dbtest/dbconfig/DatabaseLoggingAspect.java
@@ -0,0 +1,36 @@
+package com.dbtest.dbconfig;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author m.Shahrestanaki @createDate 11/26/2024
+ */
+
+@Aspect
+@Component
+@Slf4j
+public class DatabaseLoggingAspect {
+    private final DataSource dataSource;
+
+    public DatabaseLoggingAspect(@Qualifier("lazyDataSource") DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Before("execution(* org.springframework.data.repository.CrudRepository.*(..))")
+    public void logDatabaseConnection(JoinPoint joinPoint) {
+        try (Connection connection = dataSource.getConnection()) {
+            log.info("Connection URL: {} and User: {} with Executing SQL: {}", connection.getMetaData().getURL(), connection.getMetaData().getUserName(), joinPoint.getSignature().toShortString());
+        } catch (SQLException e) {
+            log.error("Error while getting connection info", e);
+        }
+    }
+}
